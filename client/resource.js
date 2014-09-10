9830b072225ade9f176864ef82551766108a49c0
katzchang <katzchang@gmail.com>
2014-09-10 23:30:50 +0900

    cors

---
+++ client/resource.js
@@ -0,0 +1,1 @@
+alert('Hello from resource.js on origin');
---
+++ server/resource.php
@@ -0,0 +1,6 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Credentials: true');
+
+echo "alert('Hello from resource.js on thirdparty');";
