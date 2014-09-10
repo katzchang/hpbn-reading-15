aac8ff2b6949cebf25d51c9ff6f877fc9c758706
katzchang <katzchang@gmail.com>
2014-09-11 01:21:46 +0900

    fix

--- origin/upload.php
+++ origin/upload.php
@@ -1,11 +1,5 @@
 <?php
-// cookie　を使うために必要なあれ
-header('Access-Control-Allow-Origin: http://localhost:8888');
-header('Access-Control-Allow-Credentials: true');
-
-echo print_r($_REQUEST, true);
-echo "\n";
-
 $i = file_get_contents('php://input');
-echo "php://input : [$i]";
-echo "\n";
+error_log($i);
+
+echo "recieved.";
